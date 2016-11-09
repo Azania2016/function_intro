4e1f974acebf48d661c6f47d60c5696af9a728ac
Azania2016 <Buzanimgamlele@gmail.com>
2016-11-09 15:20:29 +0200

    Traffic jam is bad CapeTown.

---
+++ countRegNumber.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+var countRegNumber = function (string){
+  var splitsString = string.split(",");
+  var lengthofString = splitsString.length;
+  return lengthofString;
+}
+
+assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328'), 3);
