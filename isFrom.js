8c86d06ec6a7c6baf06701a315d0a1f0bde263a3
Azania2016 <Buzanimgamlele@gmail.com>
2016-11-16 12:34:50 +0200

    Correct code of this exercise

--- isFrom.js
+++ isFrom.js
@@ -4,5 +4,5 @@ var  isFrom = function(regNo,location){
   return  true;
 };
 assert.equal(isFrom('CA 2345','CY 233'),true);
-assert.equal(isFrom('ND 999'),true);
+assert.equal(isFrom('ND 999','ND'),true);
 assert.equal(isFrom('CY 9977','CY'),true)
